c2cdb74a683ce2ac29446450be1698ceb90bcc0c
melalex <melalex490@gmail.com>
2017-04-01 18:34:12 +0300

    Horse actions

--- src/actions/Horse.js
+++ src/actions/Horse.js
@@ -2,25 +2,178 @@
  * Created by Alexander Melashchenko on 4/1/17.
  */
 
+import {ajax} from "jquery";
+import API_DOMAIN from '../constants/Api'
+
+import {
+    CREATE_HORSE_REQUEST,
+    CREATE_HORSE_SUCCESS,
+    CREATE_HORSE_FAILED,
+    GET_HORSES_REQUEST,
+    GET_HORSES_SUCCESS,
+    GET_HORSES_FAILED,
+    SEARCH_HORSES_REQUEST,
+    SEARCH_HORSES_SUCCESS,
+    SEARCH_HORSES_FAILED,
+    UPDATE_HORSE_REQUEST,
+    UPDATE_HORSE_SUCCESS,
+    UPDATE_HORSE_FAILED,
+    DELETE_HORSE_REQUEST,
+    DELETE_HORSE_SUCCESS,
+    DELETE_HORSE_FAILED
+} from '../constants/Horse'
+
 let createHorse = horse => {
-  return {
-      type: "CREATE_HORSE",
-      horse
-  }
+    return dispatch => {
+        dispatch({
+            type: CREATE_HORSE_REQUEST,
+            payload: horse
+        });
+
+        ajax({
+            type: 'POST',
+            url: API_DOMAIN + '/api/horse',
+            dataType: 'json',
+            data: horse,
+            success: response => {
+                if (response.status === 'error') {
+                    dispatch({
+                        type: CREATE_HORSE_FAILED,
+                        payload: response.result
+                    })
+                } else {
+                    dispatch({
+                        type: CREATE_HORSE_SUCCESS,
+                        payload: response.result
+                    })
+                }
+            }
+        });
+    }
+};
+
+let getHorses = page => {
+    return dispatch => {
+        dispatch({
+            type: GET_HORSES_REQUEST,
+            payload: page
+        });
+
+        ajax({
+            type: 'GET',
+            url: API_DOMAIN + '/api/horse',
+            dataType: 'json',
+            data: {
+                page: page
+            },
+            success: response => {
+                if (response.status === 'error') {
+                    dispatch({
+                        type: GET_HORSES_FAILED,
+                        payload: response.result
+                    })
+                } else {
+                    dispatch({
+                        type: GET_HORSES_SUCCESS,
+                        payload: response.result
+                    })
+                }
+            }
+        });
+    }
+};
+
+let searchHorse = (req, page) => {
+    return dispatch => {
+        dispatch({
+            type: SEARCH_HORSES_REQUEST,
+            payload: {
+                req: req,
+                page: page
+            }
+        });
+
+        ajax({
+            type: 'GET',
+            url: API_DOMAIN + '/api/horse',
+            dataType: 'json',
+            data: {
+                req: req,
+                page: page
+            },
+            success: response => {
+                if (response.status === 'error') {
+                    dispatch({
+                        type: SEARCH_HORSES_FAILED,
+                        payload: response.result
+                    })
+                } else {
+                    dispatch({
+                        type: SEARCH_HORSES_SUCCESS,
+                        payload: response.result
+                    })
+                }
+            }
+        });
+    }
 };
 
 let updateHorse = horse => {
-  return {
-      type: "UPDATE_HORSE",
-      horse
-  }
+    return dispatch => {
+        dispatch({
+            type: UPDATE_HORSE_REQUEST,
+            payload: horse
+        });
+
+        ajax({
+            type: 'PUT',
+            url: API_DOMAIN + '/api/horse',
+            dataType: 'json',
+            data: horse,
+            success: response => {
+                if (response.status === 'error') {
+                    dispatch({
+                        type: UPDATE_HORSE_FAILED,
+                        payload: response.result
+                    })
+                } else {
+                    dispatch({
+                        type: UPDATE_HORSE_SUCCESS,
+                        payload: response.result
+                    })
+                }
+            }
+        });
+    }
 };
 
 let deleteHorse = horse => {
-  return {
-      type: "DELETE_HORSE",
-      horse
-  }
+    return dispatch => {
+        dispatch({
+            type: DELETE_HORSE_REQUEST,
+            payload: horse
+        });
+
+        ajax({
+            type: 'DELETE',
+            url: API_DOMAIN + '/api/horse',
+            dataType: 'json',
+            data: horse,
+            success: response => {
+                if (response.status === 'error') {
+                    dispatch({
+                        type: DELETE_HORSE_FAILED,
+                        payload: response.result
+                    })
+                } else {
+                    dispatch({
+                        type: DELETE_HORSE_SUCCESS,
+                        payload: response.result
+                    })
+                }
+            }
+        });
+    }
 };
 
-export {createHorse, updateHorse, deleteHorse}
+export {createHorse, getHorses, searchHorse, updateHorse, deleteHorse}
---
+++ src/constants/Api.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Alexander Melashchenko on 4/1/17.
+ */
+
+const API_DOMAIN = 'http://localhost::8080';
+
+export default API_DOMAIN;
