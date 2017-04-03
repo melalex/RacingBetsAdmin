3a6e3f3d97500592e0212f19091f9d15889bdcb5
melalex <melalex490@gmail.com>
2017-04-04 01:13:02 +0300

    App component frame

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react';
-import '../../styles/App.css';
+/**
+ * Created by Alexander Melashchenko on 4/3/17.
+ */
 
+import React, {Component} from 'react';
+import {Navbar, NavbarBrand, Nav, NavItem, NavLink, Nav} from 'reactstrap';
+import alert from "bootstrap/js/src/alert";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
+export default class App extends Component {
+    logout(e) {
+        e.preventDefault();
+        alert('logout')
+    }
 
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Navbar color="faded" light>
+                    <NavbarBrand href="/">Racing Bets Admin</NavbarBrand>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            User Name
+                        </NavItem>
+                        <NavItem>
+                            <NavLink onClick={this.logout}>Logout</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Navbar>
+                {this.props.children}
+            </div>
+        )
+    }
 }
-
-export default App;
