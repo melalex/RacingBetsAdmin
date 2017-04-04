1cbd56e9efb8eb8924d9616267793859e0574f9c
melalex <melalex490@gmail.com>
2017-04-04 13:46:39 +0300

    Error handling

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,17 +2,19 @@
  * Created by Alexander Melashchenko on 4/3/17.
  */
 
-import React, {Component} from 'react';
-import {Navbar, NavbarBrand, Nav, NavItem, NavLink, Nav} from 'reactstrap';
+import React from 'react';
+import {connect} from 'react-redux'
+import {Navbar, NavbarBrand, Nav, NavItem, NavLink, Nav, UncontrolledAlert} from 'reactstrap';
 import alert from "bootstrap/js/src/alert";
 
-export default class App extends Component {
+class App extends React.Component {
     logout(e) {
         e.preventDefault();
         alert('logout')
     }
 
     render() {
+        let {errors, info} = this.props;
         return (
             <div>
                 <Navbar color="faded" light>
@@ -26,8 +28,25 @@ export default class App extends Component {
                         </NavItem>
                     </Nav>
                 </Navbar>
+                {
+                    errors.length > 0
+                        ? errors.map(e => <UncontrolledAlert color="danger"><strong>{e}</strong></UncontrolledAlert>)
+                        : null
+                }
+                {
+                    info ? (<UncontrolledAlert color="success"><strong>{info}</strong></UncontrolledAlert>) : null
+                }
                 {this.props.children}
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        errors: state.crud.errors,
+        info: state.crud.info,
+    }
+}
+
+export default connect(mapStateToProps)(App)
